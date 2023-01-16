b9934da49fcf6e378d53d05bdc6b416aabef09a2
joshuashaver <josh.shaver@ymail.com>
2023-01-15 18:55:33 -0600

    implement async channel for game endpoint

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ var upgrader = websocket.Upgrader{} // use default options
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade:", err)
+		log.Print("Error upgrading http request to websocket:", err)
 		return
 	}
 	//  Metric Instrumentation
@@ -42,6 +42,47 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func serverListenWrite(c *websocket.Conn, ch chan *[]byte) {
+	defer c.Close()
+
+	for {
+		message := <-ch
+		err := c.WriteMessage(1, *message)
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("game sent: %s", message)
+	}
+}
+
+func serverListenRead(c *websocket.Conn) {
+	defer c.Close()
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("game server recv: %s", message)
+
+		// echo for now, but this should send inputs somewhere to be calculated and forwarded to all clients
+		gameChan <- &message
+	}
+}
+
+func game(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("Error upgrading http request to websocket:", err)
+		return
+	}
+
+	go serverListenRead(c)
+	serverListenWrite(c, gameChan)
+}
+
 func healthCheckPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
 }
@@ -54,6 +95,9 @@ func serverStatsPage(w http.ResponseWriter, r *http.Request) {
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var metricsEnabled = flag.Bool("metricsEnabled", true, "flag to enable metrics")
 
+// test global channel
+var gameChan chan *[]byte
+
 func main() {
 	fmt.Println("Hello, this is the POC game server.")
 	flag.Parse()
@@ -63,9 +107,11 @@ func main() {
 		Addr:           *addr,
 		MetricsEnabled: *metricsEnabled,
 	})
-
+	gameChan = make(chan *[]byte)
 	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/game", game)
 	http.HandleFunc("/health", healthCheckPage)
 	http.HandleFunc("/stats", serverStatsPage)
 	log.Fatal(http.ListenAndServe(*addr, nil))
+
 }
